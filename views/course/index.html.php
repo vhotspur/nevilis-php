0c4788280144040696baaebbfc3ed791feba012c
Vojtech Horky <vojtech.horky@gmail.com>
2014-09-02 15:26:12 +0200

    Display comments in overview table

--- views/course/index.html.php
+++ views/course/index.html.php
@@ -3,15 +3,17 @@
 		<tr>
 			<th>Assignment name</th>
 			<th>Grade</th>
+			<th>Comment</th>
 		</tr>
 	</thead>
 	<tbody>
 <?php
 foreach ($assignments as $a) {
 	$grade = $a->grade == null ? "-" : $a->grade;
-	printf("<tr><td><a href=\"%s\">%s</a> (%d)</td><td>%s</td></tr>\n",
+	$comment = $a->comment == null ? "" : $a->comment;
+	printf("<tr><td><a href=\"%s\">%s</a> (%d)</td><td>%s</td><td>%s</td></tr>\n",
 		url_for($course_id, $a->aid),
-		h($a->name), $a->filecount, $grade);
+		h($a->name), $a->filecount, $grade, h($comment));
 }
 ?>
 	</tbody>
