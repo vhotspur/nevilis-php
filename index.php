6ffcca02267a8090bbe64cb33232f4ec6ed907d0
Vojtech Horky <vojtech.horky@gmail.com>
2014-09-07 18:55:32 +0200

    Dispatch everything to /login (even POSTs)

--- index.php
+++ index.php
@@ -68,6 +68,7 @@ if (auth_is_user_logged_in()) {
 	dispatch('/:course/:assignment/:filename', 'page_file_download');	
 } else {
 	dispatch_post('/login', 'page_auth_do_login');
+	dispatch_post('/*', 'page_auth_login');
 	dispatch('/*', 'page_auth_login');
 }
 
