87c79d3d731a924d198a44f982fc5618eef415f6
Vojtech Horky <vojtech.horky@gmail.com>
2014-10-09 13:34:48 +0200

    Formatting

--- controllers/main.php
+++ controllers/main.php
@@ -4,5 +4,5 @@ function page_main() {
 	set('title', _('Assignment submission system'));
 	set('courses', data_get_course_list_for_user(auth_get_current_user()));
 	
-    return html('main.html.php');
+	return html('main.html.php');
 }
