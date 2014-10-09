a2332c9848dc774b0f230c1116e40b429bf83530
Vojtech Horky <vojtech.horky@gmail.com>
2014-10-09 12:21:33 +0200

    Nicer assignment table in a course

--- views/course/index.tpl.html.php
+++ views/course/index.tpl.html.php
@@ -1,18 +1,18 @@
-<table border="1">
+<table class="assignments">
 <thead>
 	<tr>
-		<th>%_Assignment name_%</th>
-		<th>%_Grade_%</th>
-		<th>%_Deadline_%</th>
-		<th>%_Comment_%</th>
+		<th class="name">%_Assignment name_%</th>
+		<th class="grade">%_Grade_%</th>
+		<th class="deadline">%_Deadline_%</th>
+		<th class="comment">%_Comment_%</th>
 	</tr>
 </thead>
 %foreach $assignments $a%
 <tr>
-	<td><a href="%url($course_id, $a->aid)%">{$a->name/h}</a></td>
-	<td>{$a->grade|&mdash;}</td>
-	<td>{$a->deadline|&mdash;}</td>
-	<td>{$a->comment/h|}</td>
+	<td class="name"><a href="%url($course_id, $a->aid)%">{$a->name/h}</a></td>
+	<td class="grade">{$a->grade|&mdash;}</td>
+	<td class="deadline">{$a->deadline|&mdash;}</td>
+	<td class="comment">{$a->comment/h|}</td>
 </tr>
 %before%
 <tbody>
